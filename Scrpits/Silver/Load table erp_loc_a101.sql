9fafe39bce72bbd6e73992ee3ce48e95c8c9f76e
Youssef Ahmed <joeelnadry@gmail.com>
2026-08-18 17:23:54 +0300

    Create Load table erp_loc_a101.sql

---
+++ Scrpits/Silver/Load table erp_loc_a101.sql
@@ -0,0 +1,17 @@
+--******************** Load table: erp_loc_a101 ***********************
+INSERT INTO silver.erp_loc_a101
+(
+cid,
+cntry
+)
+
+SELECT
+REPLACE(cid,'-','')cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	 WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry IS NUll THEN 'n/a'
+	 ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101
+
+
